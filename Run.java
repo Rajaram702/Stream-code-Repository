2784f94debcb998f6965f56ff6c0de8f6b7a3dd3
Rajaram <rajatrout914@gmail.com>
2026-03-05 18:37:32 +0530

    Run class updated...

--- Run.java
+++ Run.java
@@ -1,4 +1,13 @@
+<<<<<<< Updated upstream
 public Stirng m2()
 {
 	return "Rajaram";
+=======
+public void m1(String x,int y)
+{
+	String str=x;
+	Integer itr=y;
+	String msg=x+itr.toString();
+	System.out.println(msg);
+>>>>>>> Stashed changes
 }
