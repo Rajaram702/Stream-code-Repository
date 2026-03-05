99bee35558cf7e91855ddbeb686be69277c545c1
Rajaram <rajatrout914@gmail.com>
2026-03-05 18:43:11 +0530

    Run class updated at 6:44

--- Run.java
+++ Run.java
@@ -1,13 +1,12 @@
-<<<<<<< Updated upstream
 public Stirng m2()
 {
+	System.out.println("m2 method executed....");
 	return "Rajaram";
-=======
+
 public void m1(String x,int y)
 {
 	String str=x;
 	Integer itr=y;
 	String msg=x+itr.toString();
 	System.out.println(msg);
->>>>>>> Stashed changes
 }
