b8d478c31e379f50c3e282cc7914bc67292426d5
Rajaram <rajatrout914@gmail.com>
2026-03-05 18:48:16 +0530

    Run class added..

--- Run.java
+++ Run.java
@@ -3,10 +3,12 @@ public Stirng m2()
 	System.out.println("m2 method executed....");
 	return "Rajaram";
 
-public void m1(String x,int y)
+
+public String m1(String x,int y)
 {
 	String str=x;
 	Integer itr=y;
 	String msg=x+itr.toString();
 	System.out.println(msg);
+    return msg;
 }
